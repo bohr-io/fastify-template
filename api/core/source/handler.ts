654877bae3cf346843b43d9ae85143ed60205c6f
gugahnstn <gustavonstn@hotmail.com>
2023-09-07 19:43:44 -0300

    feat: Importing the 'app' variable from the './app' file.

--- api/core/source/handler.ts
+++ api/core/source/handler.ts
@@ -3,9 +3,9 @@ import { DefaultHandler } from '@h4ad/serverless-adapter/lib/handlers/default';
 import { FastifyFramework } from '@h4ad/serverless-adapter/lib/frameworks/fastify';
 import { PromiseResolver } from '@h4ad/serverless-adapter/lib/resolvers/promise';
 import { ApiGatewayV2Adapter } from '@h4ad/serverless-adapter/lib/adapters/aws';
-import Fastify from './app';
+import app from './app';
 
-export const handler = ServerlessAdapter.new(Fastify)
+export const handler = ServerlessAdapter.new(app)
   .setFramework(new FastifyFramework())
   .setHandler(new DefaultHandler())
   .setResolver(new PromiseResolver())
